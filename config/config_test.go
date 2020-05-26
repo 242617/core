f46c9e8c0bb64b8c5f6b9cedfefd1d45266dfc9e
Dmitry Kapsamun <242617@gmail.com>
2020-05-26 08:41:10 +0300

    Fix tests

--- config/config_test.go
+++ config/config_test.go
@@ -1,26 +1,30 @@
 package config
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/242617/core/config/source"
+	"github.com/242617/core/config/source/file"
 	"github.com/pkg/errors"
 )
 
 type Item struct {
 	User struct {
 		Name struct {
-			First  string `env:"USER_FIRST_NAME" yaml:"first"`
+			First  string `env:"USER_FIRST_NAME"`
 			Second string
-		} `yaml:"name"`
+		}
 		Age     uint    `env:"USER_AGE"`
 		Balance float64 `env:"USER_BALANCE" default:"10.25"`
-		Active  bool    `env:"USER_ACTIVE" yaml:"active" default:"true"`
-	} `env:"USER" yaml:"user"`
-	Status  string        `yaml:"status" default:"ok"`
+		Active  bool    `env:"USER_ACTIVE" default:"true"`
+	} `env:"USER"`
+	Status  string        `yaml:"status_string" default:"ok"`
 	Timeout time.Duration `env:"TIMEOUT" default:"10s"`
 }
 
@@ -90,48 +94,46 @@ func TestEnvBasic(t *testing.T) {
 	}
 }
 
-// func TestYAMLBasic(t *testing.T) {
-// 	content := []byte(strings.Join([]string{
-// 		"user:",
-// 		"   name:",
-// 		"       first: Ivan",
-// 		"   active: true",
-// 		"status: idle",
-// 	}, "\n"))
+func TestYAMLBasic(t *testing.T) {
+	content := []byte(strings.Join([]string{
+		"user:",
+		"   name:",
+		"       first: Ivan",
+		"   active: true",
+		"status_string: idle",
+	}, "\n"))
 
-// 	dir, err := ioutil.TempDir(os.TempDir(), "config")
-// 	if err != nil {
-// 		t.Fatal(errors.Wrap(err, "cannot create temp directory"))
-// 	}
-// 	defer os.RemoveAll(dir)
+	dir, err := ioutil.TempDir(os.TempDir(), "config")
+	if err != nil {
+		t.Fatal(errors.Wrap(err, "cannot create temp directory"))
+	}
+	defer os.RemoveAll(dir)
 
-// 	filename := filepath.Join(dir, "config.yaml")
-// 	if err := ioutil.WriteFile(filename, content, 0666); err != nil {
-// 		t.Fatal(errors.Wrap(err, "cannot write file"))
-// 	}
+	filename := filepath.Join(dir, "config.yaml")
+	if err := ioutil.WriteFile(filename, content, 0666); err != nil {
+		t.Fatal(errors.Wrap(err, "cannot write file"))
+	}
 
-// 	var cfg Item
+	var cfg Item
 
-// 	config := New().With(file.YAML(filename))
-// 	if err := config.Scan(&cfg); err != nil {
-// 		t.Fatal(errors.Wrap(err, "cannot scan config"))
-// 	}
+	config := New().With(file.YAML(filename))
+	if err := config.Scan(&cfg); err != nil {
+		t.Fatal(errors.Wrap(err, "cannot scan config"))
+	}
 
-// 	if cfg.User.Name.First != "Ivan" {
-// 		log.Fatalf("unexpected user first name: want %q, got %q", "Vasily", cfg.User.Name.First)
-// 	}
+	if cfg.User.Name.First != "Ivan" {
+		log.Fatalf("unexpected user first name: want %q, got %q", "Vasily", cfg.User.Name.First)
+	}
 
-// 	if !cfg.User.Active {
-// 		log.Fatalf("unexpected user activity: want %t, got %t", true, cfg.User.Active)
-// 	}
+	if !cfg.User.Active {
+		log.Fatalf("unexpected user activity: want %t, got %t", true, cfg.User.Active)
+	}
 
-// 	if cfg.Status != "idle" {
-// 		log.Fatalf("unexpected status: want %q, got %q", "idle", cfg.Status)
-// 	}
+	if cfg.User.Balance != 10.25 {
+		log.Fatalf("unexpected user balance: want %f, got %f", 10.25, cfg.User.Balance)
+	}
 
-// 	if cfg.User.Balance != 10.25 {
-// 		log.Fatalf("unexpected user balance: want %f, got %f", 10.25, cfg.User.Balance)
-// 	}
-// }
-
-// func TestJSONBasic(t *testing.T) {}
+	if cfg.Status != "idle" {
+		log.Fatalf("unexpected status: want %q, got %q", "idle", cfg.Status)
+	}
+}
